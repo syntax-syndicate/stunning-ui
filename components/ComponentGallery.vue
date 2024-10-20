de26250a854fbcdb3b1556662bb27e53bd6c1d7e
Robert Shaw <xiaoluoboding@gmail.com>
2024-10-20 13:41:04 +0800

    docs: remove preview of components page

--- components/ComponentGallery.vue
+++ components/ComponentGallery.vue
@@ -11,31 +11,11 @@
           v-for="item in group.components"
           @click="navigateTo(item.path)"
         >
-          <template v-if="item.image">
-            <img
-              :src="item.image"
-              class="my-0 rounded-lg group-hover/card:scale-[1.02] transition-all duration-200"
-            />
-          </template>
-          <div
-            class="relative bg-neutral-900/5 dark:bg-white/5 ring-1 ring-inset ring-neutral-900/10 dark:ring-white/10 rounded-lg p-3"
-            v-else
-          >
-            <div
-              class="aspect-video rounded-lg relative overflow-hidden border border-dashed border-neutral-900/10 dark:border-white/10"
-            >
-              <ImagePlaceholder />
-              <div
-                className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 whitespace-pre-wrap text-center text-3xl xl:text-xl font-medium tracking-tighter text-black dark:text-white"
-              >
-                Comming Soon
-              </div>
-            </div>
-          </div>
-
           <div class="relative pt-2 text-neon-wb">
             <h1 class="font-medium text-lg mb-1">{{ item.name }}</h1>
-            <span class="line-clamp-3 text-sm">{{ item.desc }}</span>
+            <span class="line-clamp-3 text-sm">{{
+              item.desc || 'Upcoming'
+            }}</span>
           </div>
         </GlowyCard>
       </GlowyCardWrapper>
@@ -48,7 +28,6 @@ import GlowyCardWrapper from '~/components/stunning/GlowyCard/GlowyCardWrapper.v
 import GlowyCard from '~/components/stunning/GlowyCard/GlowyCard.vue'
 
 import { componentList } from '~/lib/constants'
-import ImagePlaceholder from './ImagePlaceholder.vue'
 </script>
 
 <style scoped></style>
